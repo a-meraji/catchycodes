1f6ace3ac3b1303967ded8e513015965eb49ab36
a-meraji <merajiamin1997@gmail.com>
2024-05-17 14:56:02 +0330

    grid back ground fixed

---
+++ components/services.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <div className="flex text-black z-20 items-center justify-between">
+      <div className="flex flex-col items-start">
+        <div className="">services</div>
+        <div>How we build beautiful products in constrained environments</div>
+        <div>
+          Think Company designs and builds digital tools that make work and life
+          easier. Here’s how we work with businesses like yours.
+        </div>
+      </div>
+      <img src="https://www.thinkcompany.com/wp-content/uploads/2023/02/services-graphic-light-500x487.png" />
+    </div>
+  );
+}
