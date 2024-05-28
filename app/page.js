b1b7b90696f943dba131d0ae8bb17e581a1dad0f
sepehrsdgh <sepehrdesdgh@gmail.com>
2024-05-27 20:23:11 -0700

    add contact us button

--- app/page.js
+++ app/page.js
@@ -1,23 +1,27 @@
+"use client";
 import AboutUs from "@/components/AboutUs";
 import AllServiceDetail from "@/components/AllServiceDetail";
+import ContactUs from "@/components/ContactUs";
 import Footer from "@/components/Footer";
 import ServiceDetail from "@/components/ServiceDetail";
 import AutomateCarousel from "@/components/aoutomateCarousel";
 import Intro from "@/components/intro";
 import Services from "@/components/services";
 import Us from "@/components/us";
+import { Transition } from "@/lib/utils";
+import { useState } from "react";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="relative">
       <Intro />
-       <Us />
+      <Us />
       <AutomateCarousel />
       <Services />
       <AllServiceDetail />
       <AboutUs />
-      <Footer /> 
-     
+      <Footer />
+      <ContactUs />
     </main>
   );
 }
